74b878b062169b76e0d229ceb2572d05644dd89d
Jinming3 <93009697+Jinming3@users.noreply.github.com>
2024-11-27 10:55:26 +0100

    Add files via upload

--- framework_start.py
+++ framework_start.py
@@ -3,97 +3,166 @@ framework main file, start here
 """
 import time
 import os
+import sys
 import matplotlib.pyplot as plt
-# plt.style.use('F:/Project/head/tight.mplstyle')
 import matplotlib
 import torch
-matplotlib.use("TkAgg")
 import numpy as np
-import sys
-# sys.path.append('framework/')
-from framework.framework_process import sys_select, process    #   # os.path.abspath(os.getcwd())
-sys.path.append('user/')
+from framework.framework_process import sys_select, param_save, param_load, process
+
+matplotlib.use("TkAgg")
 # ----- user import >>> ------
-import user_process
-from user_process import ModelTrain
+sys.path.append('user/')
+from user import init
+
+# ------------------ choose a system to generate data
+sys_name = 'EMPS'
+# sys_name = 'rlc'
+# sys_name = 'springs'
+# sys_name = 'tanks'
 
 
-sys_name = 'EMPS'  # choose a system to generate data
-dt = ModelTrain.dt # define the system sampling rate, setting in user_process ModelTrain
-signal = 'sinwave'  # universal, 'triangel' or 'sinwave'
-change1 = ModelTrain.change1
-change2 = ModelTrain.change2
-time_train = ModelTrain.time_train
-time_test = ModelTrain.time_test  #100  # 50
-sample = sys_select(sys_name, dt)
+# ---------------------------choose your method to test
+# module = 'nn'
+# module = 'nn_overflow' # NN for tanks overflow
+# module = 'nn_ss' # for non-mechanical like RLC, neural network state-space model
+module = 'ss'  # state space
+# module = 'narmax'
 
-# ------------ training ---------
-# data_sample_train = sample.sample(A=1, time_all=time_train, signal=signal)  # data generation for training, [Y, U]
-# user_params = user_process.train(data_sample_train)
-# model_folder = os.path.join("models", sys_name)
-# if not os.path.exists(model_folder):
-#     os.makedirs(model_folder)
-# torch.save(user_params, os.path.join(model_folder, "offline"))
-# # ------- or load ----
-model_folder = os.path.join("models", sys_name)
-user_params = torch.load(os.path.join(model_folder, "offline"))  # load saved model
-simulation = process(plot=True)  # to evaluate the user model
+# module = 'regulator'
+# module = 'regulator_overflow'  # for tanks overflow
+# module = 'regulator_nn_ss' # for non-mechanical like RLC
 
-# user_model = user_process.test
-# sample = sys_select(sys_name, dt)
-# data_sample_inference = sample.sample_infer(A=1, time_all=time_test, signal=signal, noise=10e-5)  # data for testing, only contains data_U
-# simulation.inference(user_model, user_params, data_sample_inference, measure=sample, condition='test')
+# -------------------------- choose to train the model from start or test from loading
+new_train = False
+# new_train = True
 
-sample = sys_select(sys_name, dt)
-user_model = user_process.regulator
-data_sample_aging = sample.sample_aging(A=1, change1=change1, change2=change2, time_all=time_test, signal=signal, aging_rate=0.9)
-simulation.inference(user_model, user_params, data_sample_aging, measure=sample, condition='dynamic', case=5)
+setup = init.ModelTrain_setting(sys_name) # setup hyperparameters
 
-sample = sys_select(sys_name, dt)
-user_model = user_process.regulator
-data_sample_ref = sample.sample_ref(A=1, change1=change1, change2=change2, time_all=time_test)  # data_sample only contains data_U. ref change
-simulation.inference(user_model, user_params, data_sample_ref, measure=sample, condition='dynamic', case=5)
+norm = 1 # normalize data into [-norm, +norm]
+
+# # ------------ training ---------
+if new_train:
+    sample = sys_select(sys_name, setup)
+    data_sample_train = sample.sample(time_all=setup.time_train, norm=norm, noise=0)  # data pair for training, [Y, U]
+
+    exec(f'from {module} import train')
+    user_params = train(data_sample_train, setup)
+
+    param_save(user_params, sys_name, module)
+# # -------  load for trained module----
+if not new_train:
+    user_params = param_load(sys_name, module)
+
+simulation = process(plot=True)  # to evaluate your model
+sample = sys_select(sys_name, setup)
+data_sample_test = sample.sample_test(time_all=setup.time_test, norm=norm,
+                                            noise=10e-5)  # data for testing, only contains data_U
+exec(f'from {module} import test')
+user_model = test  # using testing dataset
+simulation.test(user_model, user_params, data_sample_test, setup, measure=sample, condition='test')
 
 
+# system under sudden impacts
+sample = sys_select(sys_name, setup)
+data_sample_change = sample.sample_change(change=setup.change, time_all=setup.time_test, norm=norm) # only U
+simulation.test(user_model, user_params, data_sample_change, setup, measure=sample, condition='dynamic')  #
 
-# #
 
+# exec(f'from {module} import regulator')
+# user_model = regulator
+# simulation.test(user_model, user_params, data_sample_change, setup,measure=sample, condition='dynamic')
+
+# # # # # # test for n-step-ahead prediction, done for SSM --
+# sample = sys_select(sys_name,setup)
+# data_sample_test = sample.sample_test(time_all=setup.time_test, norm=norm, noise=10e-4) #  # data for testing, only contains data_U
+# ahead_step= 10#30#5#100
+# user_model = test
+# simulation.test(user_model, user_params, data_sample_test, setup, measure=sample, condition='ahead', ahead_step=ahead_step)
+#  ------------here finishes n-step-ahead #----------
+
+# # # # ---- varying ahead step test ------
+# ahead_step_range = [5, 10, 20, 30, 40, 50, 100]
 #
-# # model change to regulator
-# # simulation = process(plot=True)  # to evaluate the user model
+# predict_ahead_error =[]
+# simulation = process(plot=False)  # to evaluate the user model
 #
-# data_sample_aging = sample.sample_aging(A=1, change1=change1, change2=change2, time_all=time_test, signal=signal, aging_rate=0.9)
-# # np.savetxt('data_sample_aging.txt', (data_sample_aging, sample.Y_hidden),delimiter=',')
-# user_model = user_process.regulator
-# simulation.inference(user_model, user_params, data_sample_aging, measure=sample, condition='dynamic')
-# yhat_regulator = simulation.yhat
-
-# rate_evl = [0.99,0.97,0.95,0.93, 0.9,0.87,0.85, 0.83,0.8,0.75, 0.7, 0.65,0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
-# r2_evl = []
-# rmse_evl=[]
-# for r in rate_evl:
+# for ahead_step in ahead_step_range:
+#     sample = sys_select(sys_name, setup)
+#     data_sample_test = sample.sample_test(time_all=setup.time_test, norm=norm,
+#                                                 noise=10e-4)  # # data for testing, only contains data_U
+#     exec(f'from {module} import test')
+#     user_model = test
 #
-#     sample = sys_select(sys_name, dt)
-#     data_sample_aging = sample.sample_aging(A=1, change1=10, change2=30, time_all=time_test, signal=signal, aging_rate=r)
-#     simulation.inference(user_model, user_params, data_sample_aging, measure=sample, condition='dynamic', ahead_step=1)
+#     simulation.test(user_model, user_params, data_sample_test, setup, measure=sample, condition='ahead',
+#                          ahead_step=ahead_step)
+#     predict_ahead_error.append([simulation.RMSE_ahead, simulation.r2_ahead, simulation.fit_ahead])
+# predict_ahead_error = np.array(predict_ahead_error, dtype=np.float32)
 #
+# # np.savetxt(f'ahead_error_{sys_name}_{module}.txt', predict_ahead_error)
 #
-#     r2_evl.append(simulation.r2)
-#     rmse_evl.append(simulation.RMSE)
-#
-# np.savetxt(f"aging_rate_{'%.4f' %dt}.txt", [rate_evl, r2_evl, rmse_evl])
-# rate_evl_plot = [1-p for p in rate_evl]
 # fig, ax = plt.subplots(2, 1, sharex=True)
-# ax[0].plot(rate_evl_plot,r2_evl, 'k', label='$R^2$')
-# ax[0].grid()
+# ax[0].plot(ahead_step_range, predict_ahead_error[:, 0], 'kx-', label='rmse')
 # ax[0].legend()
-# ax[1].plot()
-# ax[1].plot(rate_evl_plot,rmse_evl, 'k', label='RMSE')
-# ax[1].set_xlabel('aging rate')
-# ax[1].legend()
-# ax[1].grid()
 
-# -----------
+# ax[1].plot(ahead_step_range,predict_ahead_error[:, 1],'bx-', label='r2')
+# ax[1].legend()
+
+# # # -----------plot here--------------------
+# ahead_step_range = [5, 10, 20, 30, 40, 50, 100]
+# error_tanks_tanks = np.loadtxt('ahead_error_tanks_tanks.txt')
+# error_tanks_ss = np.loadtxt('ahead_error_tanks_state_space.txt')
+# error_tanks_narmax = np.loadtxt('ahead_error_tanks_narmax.txt')
+#
+# error_emps_nn = np.loadtxt('ahead_error_EMPS_NN.txt')
+# error_emps_ss = np.loadtxt('ahead_error_EMPS_state_space.txt')
+# error_emps_narmax = np.loadtxt('ahead_error_EMPS_narmax.txt')
+#
+# error_springs_nn = np.loadtxt('ahead_error_springs_NN.txt')
+# error_springs_ss = np.loadtxt('ahead_error_springs_state_space.txt')
+# error_springs_narmax = np.loadtxt('ahead_error_springs_narmax.txt')
+#
+# # --- plot all ---
+# fig, ax = plt.subplots(3, 1, sharex=True)
+# ax[0].plot(ahead_step_range, error_emps_nn[:, 0], 'rx-', label='nn')
+# ax[0].plot(ahead_step_range, error_emps_ss[:, 0], 'bx-', label='ss')
+# ax[0].plot(ahead_step_range, error_emps_narmax[:, 0], 'yx-', label='narmax')
+# ax[0].legend()
+# ax[0].set_xlabel('EMPS')
+# ax[1].plot(ahead_step_range, error_springs_nn[:, 0], 'rx-', label='nn')
+# ax[1].plot(ahead_step_range, error_springs_ss[:, 0], 'bx-', label='ss')
+# ax[1].plot(ahead_step_range, error_springs_narmax[:, 0], 'yx-', label='narmax')
+# ax[1].legend()
+# ax[1].set_xlabel('springs')
+# ax[2].plot(ahead_step_range, error_tanks_tanks[:, 0], 'rx-', label='nn')
+# ax[2].plot(ahead_step_range, error_tanks_ss[:, 0], 'bx-', label='ss')
+# ax[2].plot(ahead_step_range, error_tanks_narmax[:, 0], 'yx-', label='narmax')
+# ax[2].legend()
+# ax[2].set_xlabel('tanks')
+# fig.suptitle('rmse')
+#
+# fig, ax = plt.subplots(3, 1, sharex=True)
+# ax[0].plot(ahead_step_range, error_emps_nn[:, 1], 'rx-', label='nn')
+# ax[0].plot(ahead_step_range, error_emps_ss[:, 1], 'bx-', label='ss')
+# ax[0].plot(ahead_step_range, error_emps_narmax[:, 1], 'yx-', label='narmax')
+# ax[0].legend()
+# ax[0].set_xlabel('EMPS')
+# ax[1].plot(ahead_step_range, error_springs_nn[:, 1], 'rx-', label='nn')
+# ax[1].plot(ahead_step_range, error_springs_ss[:, 1], 'bx-', label='ss')
+# ax[1].plot(ahead_step_range, error_springs_narmax[:, 1], 'yx-', label='narmax')
+# ax[1].legend()
+# ax[1].set_xlabel('springs')
+# ax[2].plot(ahead_step_range, error_tanks_tanks[:, 1], 'rx-', label='nn')
+# ax[2].plot(ahead_step_range, error_tanks_ss[:, 1], 'bx-', label='ss')
+# ax[2].plot(ahead_step_range, error_tanks_narmax[:, 1], 'yx-', label='narmax')
+# ax[2].legend()
+# ax[2].set_xlabel('tanks')
+# fig.suptitle('r2')
+
+
+
+
+
 
 
 
